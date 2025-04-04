37e9cdd62c0cd64040e9d604d407695d16f12d18
Ismael Pintos <ismaelp@qualabs.com>
2025-04-04 15:28:23 -0300

    Use different ads

--- examples/nextjs-with-typescript/pages/playlist-page.tsx
+++ examples/nextjs-with-typescript/pages/playlist-page.tsx
@@ -21,7 +21,7 @@ function MuxVideoPage() {
       imageUrl: "https://image.mux.com/DS00Spx1CV902MCtPj5WknGlR102V5HFkDe/thumbnail.jpg",
       title: "Test video title 3",
       playbackId: "DS00Spx1CV902MCtPj5WknGlR102V5HFkDe",
-      adTagUrl: "https://pubads.g.doubleclick.net/gampad/ads?iu=/21775744923/external/single_preroll_skippable&sz=640x480&ciu_szs=300x250%2C728x90&gdfp_req=1&output=vast&unviewed_position_start=1&env=vp&impl=s&correlator=",
+      adTagUrl: "https://pubads.g.doubleclick.net/gampad/ads?iu=/21775744923/external/vmap_ad_samples&sz=640x480&cust_params=sample_ar%3Dpreonly&ciu_szs=300x250%2C728x90&gdfp_req=1&ad_rule=1&output=vmap&unviewed_position_start=1&env=vp&impl=s&correlator=",
     },
   ];
 
